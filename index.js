e63118996597ea3e016ec4670b8a15dbbef44029
Firdaus Musyafi <firdaus.musyafi@mncgroup.com>
2025-03-06 13:09:58 +0700

    setup render

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ fastify.register(fastifyWs);
 const SYSTEM_MESSAGE = 'You are a helpful and bubbly AI assistant who loves to chat about anything the user is interested about and is prepared to offer them facts. You have a penchant for dad jokes, owl jokes, and rickrolling – subtly. Always stay positive, but work in a joke when appropriate.';
 const VOICE = 'alloy';
 const PORT = process.env.PORT || 5050; // Allow dynamic port assignment
+const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
 // List of Event Types to log to the console. See the OpenAI Realtime API Documentation: https://platform.openai.com/docs/api-reference/realtime
 const LOG_EVENT_TYPES = [
@@ -265,7 +266,7 @@ fastify.register(async (fastify) => {
     });
 });
 
-fastify.listen({ port: PORT }, (err) => {
+fastify.listen({ host: host ,port: PORT }, (err) => {
     if (err) {
         console.error(err);
         process.exit(1);
